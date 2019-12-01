1ccaed7624250b2628d8f038027229a0495e08e6
Noah Sylwester <nanoabram@comcast.net>
2019-12-01 01:01:23 -0800

    Updates

---
+++ precache-manifest.34e17b95be9e13aa166534444992e041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db69f28d0dea21c0916982269a9d6bc",
+    "url": "/ImageClicker/index.html"
+  },
+  {
+    "revision": "97083e3347515a852c4a",
+    "url": "/ImageClicker/static/css/main.60596b72.chunk.css"
+  },
+  {
+    "revision": "e58340c4f86e69f67ac6",
+    "url": "/ImageClicker/static/js/2.c3748375.chunk.js"
+  },
+  {
+    "revision": "97083e3347515a852c4a",
+    "url": "/ImageClicker/static/js/main.6c7861b8.chunk.js"
+  },
+  {
+    "revision": "541707eff8de2e144185",
+    "url": "/ImageClicker/static/js/runtime-main.e7e55bcc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ImageClicker/static/media/logo.25bf045c.svg"
+  }
+]);
